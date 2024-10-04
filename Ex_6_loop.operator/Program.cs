f7f74748fc15f585eee98150fee196c39a23520c
karolinaaa2 <hryhorchuk.karolina@chnu.edu.ua>
2024-10-05 00:55:00 +0300

    Add project files.

---
+++ Ex_6_loop.operator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        string binary = Convert.ToString(number, 2);
+        Console.WriteLine($"Число {number} в двійковій системі: {binary}");
+    }
+}
